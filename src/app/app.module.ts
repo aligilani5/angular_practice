9b264db3606a853f60630784ed1d310a65ddc7ea
aligilani5 <airtaza753@gmail.com>
2022-06-22 19:53:51 +0500

    employee module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
-  ],
+],
   imports: [
     BrowserModule,
     AppRoutingModule
