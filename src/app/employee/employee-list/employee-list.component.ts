9623e2cdf372b06336b86d7fda9ab52438935b14
aligilani5 <airtaza753@gmail.com>
2022-06-23 18:29:19 +0500

    employee interface

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeList } from '../interfaces/employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  public employeeList: any[];
+  public employeeList: EmployeeList[];
 
   constructor() { 
 
---
+++ src/app/employee/interfaces/employee.ts
@@ -0,0 +1,7 @@
+export interface EmployeeList{
+
+    EmployeeId: number;
+    Name: string;
+    Gender: string;
+    Salary: number;
+}
